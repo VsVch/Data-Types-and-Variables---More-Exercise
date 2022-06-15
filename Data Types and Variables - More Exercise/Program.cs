9d4561875d1293e82e749ecb0049c54335aea378
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-15 06:33:23 +0300

    Add project files.

---
+++ Data Types and Variables - More Exercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Data_Types_and_Variables___More_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.WriteLine(Convert.ToString(input[i]));
+
+            }
+            
+
+
+
+        }
+    }
+}
